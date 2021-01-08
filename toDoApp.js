f6ea2a2539163be1bdb1f013efe9ff35e824dc7c
sachin <sachinworkk@gmail.com>
2021-01-08 10:53:35 +0545

    setup for lit element

--- toDoApp.js
+++ toDoApp.js
@@ -1,50 +1,47 @@
-const template = document.createElement('template');
+import {html,render} from 'lit-html';
 
+const template = html `
+            <style>
+            ul{
+                margin-top: 50px;
+                padding-left: 0;
+                min-width: 40%;
+                list-style: none;
+            }
+            input,button{
+                padding: 0.4rem;
+                font-size: 1.6rem;
+                background: white;
+            }
+            button{
+                color: #c065c0;
+                background: white;
+                cursor: pointer;
+                transition: all 0.3s ease;
+            }
+            form button:hover{
+                background: #c065c0;
+                color: white;
+            }
+            </style>
+            <div class="toDoList">
+            <input type="text"  placeholder="Enter a to do"></input>
+            <button>Add</button>
+            <h2> To Do's </h2>
+            <ul>
 
-template.innerHTML =`
-    <style>
-        ul{
-            margin-top: 50px;
-            padding-left: 0;
-            min-width: 40%;
-            list-style: none;
-        }
-        input,button{
-            padding: 0.4rem;
-            font-size: 1.6rem;
-            background: white;
-        }
-        button{
-            color: #c065c0;
-            background: white;
-            cursor: pointer;
-            transition: all 0.3s ease;
-        }
-        form button:hover{
-            background: #c065c0;
-            color: white;
-        }
-    </style>
-    <div class="toDoList">
-        <input type="text"  placeholder="Enter a to do"></input>
-        <button>Add</button>
-        <h2> To Do's </h2>
-        <ul>
-
-        </ul>
-    </div>
-`;
-
+            </ul>
+            </div>  
+        `;
 class ToDoApp extends HTMLElement{
     constructor(){
         super();
         this._shadowRoot = this.attachShadow({mode: 'open'});
-        this._shadowRoot.appendChild(template.content.cloneNode(true));
         this.todosList = this._shadowRoot.querySelector('ul');
         this.addButton = this._shadowRoot.querySelector('button');
         this.input= this._shadowRoot.querySelector('input');
         this.todos = [];
-        
+        render(template,this._shadowRoot);
     } 
     toggleTodo(e) {
         const todo = this.todos[e.detail];
